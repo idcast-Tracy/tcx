866d44b5a42a5ee83dc501cb21a3d207d0804417
idcast-Tracy <138656256+idcast-Tracy@users.noreply.github.com>
2024-05-21 20:01:51 +0800

    一叶知秋.py

--- 一叶知秋.py
+++ 一叶知秋.py
@@ -1,19 +1,15 @@
 # 打开网页，在cmd命令界面运行下面一段
-# # streamlit run C:\Users\Tracy\Desktop\2024Winter\科研\04.07Python-oneleaf\一叶知秋.py [ARGUMENTS]
+# # streamlit run C:\Users\Tracy\Desktop\知识图谱2.0\知识图谱.py [ARGUMENTS]
+
 
 import streamlit as st
 from streamlit_option_menu import option_menu
-from PIL import Image
 import pandas as pd
-import numpy as np
-import plotly.express as px
 import os
 import time
-import datetime
-import base64
-# os.chdir(r'C:\Users\Tracy\Desktop\2024Winter\科研\04.07Python-oneleaf') # 设定文件路径
+# os.chdir(r'C:\Users\Tracy\Desktop\知识图谱2.0') # 设定文件路径
 # 设置页面配置
-st.set_page_config(page_title="CX-copilot 1.0", page_icon="⭐", layout="wide")
+st.set_page_config(page_title="CX-copilot 2.0", page_icon="⭐", layout="wide")
 # 通过 css样式隐藏主菜单和页脚
 hide_menu = '''
 <style>
@@ -21,27 +17,11 @@ hide_menu = '''
 footer {visibility:hidden;}
 '''
 st.markdown(hide_menu,unsafe_allow_html=True)
-# st.balloons()
 
-def main_bg(main_bg):
-    main_bg_ext = "back.png"
-    st.markdown(
-        f"""
-         <style>
-         .stApp {{
-             background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
-             background-size: cover
-         }}
-         </style>
-         """,
-        unsafe_allow_html=True
-    )
-# 调用
-main_bg('back.png')
 
 with st.sidebar:
-    choose = option_menu("教师端", ["首页", "知识库补充" ,"课程管理", "学情评价","AI反馈"],
-                         icons=['bar-chart', 'cloud-upload', 'person lines fill', 'app-indicator', "boombox-fill"],
+    choose = option_menu("教师端", ["首页", "知识库补充" ,"课程管理", "学情评价","AI反馈","知识图谱"],
+                         icons=['bar-chart', 'cloud-upload', 'person lines fill', 'app-indicator', "boombox-fill","list-task"],
                          menu_icon="list", default_index=0,
                          styles={
         "container": {"padding": "5!important", "background-color": "#dceef8"}, # 整体颜色
@@ -73,6 +53,10 @@ elif choose == "AI反馈":
     with st.spinner('超星小助手正在努力加载中......'):
         time.sleep(1)
 
-
-
-
+elif choose == "知识图谱":
+    with st.spinner('超星小助手正在努力加载中......'):
+        time.sleep(1)
+    df = pd.DataFrame({'Subject': ['养生','侍奉死者','姑娘','楚文王','臣不阴','汤临川','蔡锷','少年','父母','阳','家庭','孙中山','家长制的专权','国','弟子','孔子','道理','张苍梧','凭父','凭时'],
+                       'Relationship': ['不足以当','如同','要','意欲','则','非','策划','雄于','为','不阳','会失去','发动','损害','胜于','被','批评','刚柔交错','尝语','未解','年数'],
+                       'Object': ['大事','侍奉生者','出嫁','强娶','国必乱','敌手','武力反袁','欧洲','刚','父母','动力','护法战争','家庭','欧洲','打板子','他','自然','凭父','所以','岁']})
+    edited_df = st.data_editor(df, num_rows="dynamic",height=700,width=350)
